75c78947080139337b43a2978af3bf4c21b0f583
B. P. Hon <baijiephoneycutt@gmail.com>
2023-12-17 19:06:20 -0500

    fix asset prefix 1.1

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "export", assetPrefix: "./" };
+const nextConfig = { output: "export", assetPrefix: "/" };
 
 module.exports = nextConfig;
