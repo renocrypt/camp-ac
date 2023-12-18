130c40fd4ef30c4b0d2e59d667e943128ad29972
B. P. Hon <baijiephoneycutt@gmail.com>
2023-12-17 19:56:37 -0500

    fix asset prefix 1.9.2

--- next.config.js
+++ next.config.js
@@ -2,9 +2,16 @@
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-const repo = "camp-ac";
-const assetPrefix = `/${repo}/`;
-const basePath = `/${repo}`;
+let assetPrefix = "";
+let basePath = "";
+
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
 
 const nextConfig = {
   output: "export",
