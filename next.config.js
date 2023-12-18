e297e1fd58c3c1083cff23d5806c7512e7946ff6
B. P. Hon <baijiephoneycutt@gmail.com>
2023-12-17 19:20:39 -0500

    fix asset prefix 1.4

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: "export",
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  images: { unoptimized: true },
+  basePath: "/camp-ac",
 };
 
 module.exports = nextConfig;
