69ac95177725c691b11ad7e5238ee3383e0b9938
B. P. Hon <baijiephoneycutt@gmail.com>
2023-12-17 20:05:41 -0500

    fix asset prefix 2.0

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -62,17 +62,17 @@ const Hero = () => {
               <p className="regular-16 text-gray-20">Location</p>
               <Image src="close.svg" alt="close" width={24} height={24} />
             </div>
-            <p className="bold-20 text-white">Aguas Calientes</p>
+            <p className="bold-20 text-white">Lali Baik</p>
           </div>
 
           <div className="flexBetween">
             <div className="flex flex-col">
               <p className="regular-16 block text-gray-20">Distance</p>
-              <p className="bold-20 text-white">173.28 mi</p>
+              <p className="bold-20 text-white">193.28 mi</p>
             </div>
             <div className="flex flex-col">
               <p className="regular-16 block text-gray-20">Elevation</p>
-              <p className="bold-20 text-white">2.040 km</p>
+              <p className="bold-20 text-white">2.040 mi</p>
             </div>
           </div>
         </div>
